4a0837ca423cd65f3c6dd2344547407b4f1287ed
waseshaia-01 <waseshaia@gmail.com>
2025-08-22 14:54:16 +0800

    Update scanner.js

--- js/scanner.js
+++ js/scanner.js
@@ -1,6 +1,6 @@
 // Supabase config
 const SUPABASE_URL = "https://lwsozcrubhmcvqutsfje.supabase.co";
-const SUPABASE_KEY = "YOUR-ANON-KEY-HERE";  // <-- replace with anon key
+const SUPABASE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imx3c296Y3J1YmhtY3ZxdXRzZmplIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTU4NDIxNTQsImV4cCI6MjA3MTQxODE1NH0.fx6W2L66WFdkbxC9BKvJC7AzNHE4VS8tPu4YDf3YD9E";  // <-- replace with anon key
 const { createClient } = supabase;
 const client = createClient(SUPABASE_URL, SUPABASE_KEY);
 
